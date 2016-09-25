3537f2b1ff201394eebc96a59540be716871efc7
Patrick Kennedy <mixelpix@mac.com>
2016-09-25 00:11:36 -0400

    ch1

--- ch1/7treasure.rb
+++ ch1/7treasure.rb
@@ -51,3 +51,142 @@ mycat.set_name( 'Elvis' )
 puts mycat.get_name
 
 
+
+=begin
+CONSTRUCTORS - NEW AND INITIALIZE
+when a clas contains a method named "initialize" it will automatically
+be called when an object is created using the ".new" method. The initialize
+method can set the values of an objects instance variables.
+=end
+
+
+class Thing
+
+  def set_name( aName )
+    @name     = aName       #  <-- name vs aName ?
+  end
+
+  def get_name
+    return @name
+  end
+
+end
+
+
+class Treasure
+
+  def initialize( aName, aDescription )
+    @name        = aName
+    @description = aDescription
+  end
+
+  def to_s       # <-- OVERRIDE DEFAULT to_s METHOD
+    "The #{@name} Treasure is #{@description}\n"
+  end
+
+end
+
+
+thing1 = Thing.new
+thing1.set_name( "A lovely Thing" )
+puts thing1.get_name
+
+t1 = Treasure.new("Sword", "an Elvish weapon of finest quality")
+t2 = Treasure.new("Ring", "a magic ring of grewat power")
+puts t1.to_s
+puts t2.to_s
+
+# The .inspect method lets you look inside an object
+puts "Inspecting 1st treasure: #{t1.inspect}"
+
+
+
+
+# Another stab at Animal:
+
+
+class Animal
+
+  def initialize( words )
+    @words = words
+  end
+
+  def set_name( aName )
+    @aName    = aName
+  end
+
+  def get_name
+    return @aName
+  end
+
+  def talk
+  return @words
+  end
+end
+
+class Dog < Animal
+  def initialize( word='woof' )
+    @words      = word
+  end
+end
+
+class Cat < Animal
+  def initialize( word='meow' )
+    @words      = word
+  end
+end
+
+x = Dog.new
+x.set_name('Mojo')
+puts "#{x.get_name} says, '#{x.talk}'"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
